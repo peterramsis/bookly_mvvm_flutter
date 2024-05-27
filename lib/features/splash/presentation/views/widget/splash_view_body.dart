a59cdcaf6355a90d95eb9f08c75dfc725737ab3d
PeterRamsis <peterzizo100@gmail.com>
2024-05-27 19:31:56 +0300

    splash screen

---
+++ lib/features/splash/presentation/views/widget/splash_view_body.dart
@@ -0,0 +1,29 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        const Text(
+            'Read Free Books',
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
+  }
+}
