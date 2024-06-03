2bec1c384be8e331fe5fad688f508d7561b12346
PeterRamsis <peterzizo100@gmail.com>
2024-06-04 00:33:46 +0300

    edit failure

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,64 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+
+  const Failure(this.message);
+}
+
+class ServerFailure {
+  final String message;
+  ServerFailure(this.message);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioError.response?.statusCode, dioError.response?.data);
+
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was canceled');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection error with ApiServer');
+
+      case DioExceptionType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error, Please try again!');
+
+      default:
+        return ServerFailure('Oops, there was an Error, Please try again');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int? statusCode, dynamic data) {
+    if (statusCode == null) {
+      return ServerFailure('Received invalid status code from server');
+    }
+    switch (statusCode) {
+      case 400:
+        return ServerFailure('Bad request');
+      case 401:
+        return ServerFailure('Unauthorized');
+      case 403:
+        return ServerFailure('Forbidden');
+      case 404:
+        return ServerFailure('Not found');
+      case 500:
+        return ServerFailure('Internal server error');
+      default:
+        return ServerFailure('Received invalid status code: $statusCode');
+    }
+  }
+}
